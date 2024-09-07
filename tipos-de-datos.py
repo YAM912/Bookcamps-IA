0d7d36236834daa6fe105ba417c2bdacca98ea87
YAM912 <martinezyamid@hotmail.com>
2024-09-06 21:16:34 -0500

    abordamos tipos de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,15 @@
+"string" # Se utiliza para cadenas de texto
+'string' # Asi tambien podemos generar un string
+"Hola mundo"
+'Hola mundo'
+#De la siguiente manera se genera un bloque de texto
+""" Mis datos son:
+Nombre: Yamid
+Apellidos: Martinez """
+
+# En paython tenemos diferentes tipos de numeros
+5 # Numero entero
+#Tambien tenemos el tipo de datos booleano
+50.2 #Numero tipo float
+True #Verdadero
+False #Falso
