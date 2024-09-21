e2bb0014630f97cf104420fcc8a30dfa5c5ba5c5
YAM912 <martinezyamid@hotmail.com>
2024-09-21 10:09:20 -0500

    Tratamola tematica de datos compuestos

---
+++ datos-compuestos.py
@@ -0,0 +1,36 @@
+# LISTA
+lista = ["Yamid Martinez", "Ing En Automatica industrial", "Alumno ONE en Back-end", "Alumno Talento Tech IA", "Cursando tecnico en BigData en capacitate para el empleo",True, 1.73]
+#En el mundo de la programacion no contamos del 1 al 10, contamos del o al 9
+print (lista[4])
+lista [4] = "Cursando tecnico en Big Data en la pagina Capacitate para el emple FCS"
+print(lista[4])
+
+# TUPLA : Es una lista que no se puede modificar
+tupla = ("Cafe", "pan con mantequilla", "Huevos", True, 8.50)
+print(tupla[1])
+print(f"La tupla es: {tupla}")
+# Tratando de modificar la tupla, lo cual muestra error al compilar
+#tupla [0] = "Chocolate"
+#print (tupla[1])
+
+# CONJUNTO (En ingle SET)
+conjunto = {"Yamid Martinez", "Ing En Automatica industrial", "Alumno ONE en Back-end", "Alumno Talento Tech IA", "Cursando tecnico en BigData en capacitate para el empleo",True, 1.73, "Yamid Martinez"}
+print(conjunto)
+# Comentario que explica la impresion
+print(f"El conjunto es:  {conjunto}")
+
+#DICCIONARIO
+diccionario = {
+    "nombre": "Yamid",
+    "Apellido": "Martinez",
+    "Profesion": "Ing en Automatica Industrial",
+    "Estatura": 1.73,
+    "nombre": "Yamid"
+    }
+print (diccionario)
+print (f"El diccionario es: {diccionario}")
+
+
+
+
+
