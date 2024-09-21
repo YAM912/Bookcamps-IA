1a43d16d207b49066d5cab5679620315652b2063
YAM912 <martinezyamid@hotmail.com>
2024-09-20 19:35:38 -0500

    Se abordaron temas respecto a tipos de datos en Phyton

---
+++ main.py
@@ -0,0 +1,27 @@
+name = "Yamid"
+last_name = "Martinez"
+print(type(name))
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[-1])
+print (5 * name)
+print(len(name))  # Cuenta el total de caracteres de la variable
+print(name.lower()) # Pone el texto en minusculas
+print(name.upper()) # Pone el texto en mayusculas
+print(name.strip())
+
+#Tipos de datos: Enteros, flotamtes y exponenciales
+x = 10
+y = 5.678
+z = 1.2E6
+print(type(x)) #Tipo de la variable x: "int"
+print(type(y)) #Tipo de la variable y: "float"
+print(z)  # Imprime el valor de Z
+print(type(z))  ##Tipo de la variable z: "float"
+is_true = True
+is_false = False
+print(type(is_true)) #Tipo de la variable is_true: "bool"
+print(type(is_false)) #Tipo de la variable is_false: "bool"
+
+
