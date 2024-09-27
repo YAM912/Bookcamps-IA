ec1df2abd34031b0638720496c7dcacb2e9f3488
YAM912 <martinezyamid@hotmail.com>
2024-09-27 18:04:41 -0500

    Operadores aritmeticos

---
+++ operadores-aritmeticos.py
@@ -0,0 +1,20 @@
+suma = 12 + 4
+resta = 12 - 4
+multiplicacion = 12 * 4
+division = 12 / 4
+potenciacion = 12 ** 4
+division_Entera = 12 // 4
+resto = 12 / 4
+print (suma, resta, multiplicacion, division, potenciacion, division_Entera, resto)
+tipo_de_dato = type ("Hola profe") # Devuelve el tipo de dato
+print (tipo_de_dato)
+tipo_de_dato2 = type (["Hola profe", 32, 1992]) # Devuelve el tipo de dato
+print (tipo_de_dato2)
+tipo_de_dato3 = type (("Hola profe", 32, 1992)) # Devuelve el tipo de dato
+print (tipo_de_dato3)
+tipo_de_dato4 = type (2.5) # Devuelve el tipo de dato
+print (tipo_de_dato4)
+tipo_de_dato5 = type (division) # Devuelve el tipo de dato
+print (tipo_de_dato5)
+tipo_de_dato5 = type (division_Entera) # Devuelve el tipo de dato
+print (tipo_de_dato5)
